137eeb5807ed662bb2499dc06f82411bca53aa55
Hein <hvdheever@gmail.com>
2016-05-21 17:52:33 +0200

    Second push to assignment week 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,18 +5,36 @@
 ##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  Matrix_Input <<- x
-  Output_makeCacheMatrix <<- solve(x)
-  Output_makeCacheMatrix
+  
+  x <- mat
+  m <- NULL # Empty cache
+  set <- function(y) {
+    x <<- y
+    m <<- NULL 
+  }
+  get <- function() x
+  setsolve <- function(solvematrix) m <<- solvematrix
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
 
 ## Function that checks if inverse exists in environment cache and calculates the inverse if it doesn't
 
 cacheSolve <- function(x, ...) {
-  if (identical(x, Matrix_Input) == TRUE) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
     message("getting cached data")
-    return(Output_makeCacheMatrix)}
-  Output_cacheSolve <- solve(x)
-  Output_cacheSolve
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+
+
